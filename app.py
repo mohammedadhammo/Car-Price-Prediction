318d1e4bc9d1b203974a24bd1b9cf213f126ba57
Mohammed Adham El Sayed <153763031+mohammedadhammo@users.noreply.github.com>
2025-04-14 15:07:37 +0200

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 import numpy as np
 
 # تحميل النموذج
-model = joblib.load("Car-Price-Prediction/car_price_model.pkl")
+model = joblib.load("./Car-Price-Prediction/car_price_model.pkl")
 
 # إعداد الصفحة
 st.set_page_config(page_title="Car Price Predictor", layout="centered")
